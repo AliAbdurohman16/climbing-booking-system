129b10a2456f5d8b75bd037b066c150b9b5dda11
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-03-14 20:56:15 +0700

    update views login :rocket:

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 @extends('auth.layouts.main')
 
-@section('title', 'Login')
+@section('title', 'Masuk')
 
 @section('content')
 <!-- Hero Start -->
@@ -13,10 +13,10 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
                         <a href="{{ route('/') }}"><img src="{{ asset('backend') }}/assets/images/logo-icon.png" class="avatar avatar-small mb-4 d-block mx-auto" alt=""></a>
-                        <h5 class="mb-3 text-center">Please sign in</h5>
+                        <h5 class="mb-3 text-center">Silakan masuk</h5>
 
                         <div class="form-floating mb-2">
-                            <input type="email" class="form-control @error('email') is-invalid @enderror"  name="email" id="floatingInput" placeholder="name@example.com">
+                            <input type="email" class="form-control @error('email') is-invalid @enderror"  name="email" id="floatingInput" placeholder="nama@gmail.com" value="{{ old('email') }}" autocomplete="email">
                             <label for="floatingInput">Email</label>
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -25,8 +25,8 @@
                             @enderror
                         </div>
                         <div class="form-floating mb-3">
-                            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="floatingPassword" placeholder="Password">
-                            <label for="floatingPassword">Password</label>
+                            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="floatingPassword" placeholder="Kata Sandi">
+                            <label for="floatingPassword">Kata Sandi</label>
                             @error('password')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -38,16 +38,16 @@
                             <div class="mb-3">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="flexCheckDefault" {{ old('remember') ? 'checked' : '' }}>
-                                    <label class="form-check-label" for="flexCheckDefault">Remember me</label>
+                                    <label class="form-check-label" for="flexCheckDefault">Ingat saya</label>
                                 </div>
                             </div>
-                            <p class="forgot-pass mb-0"><a href="{{ route('password.request') }}" class="text-dark small fw-bold">Forgot password ?</a></p>
+                            <p class="forgot-pass mb-0"><a href="{{ route('password.request') }}" class="text-dark small fw-bold">Lupa kata sandi ?</a></p>
                         </div>
 
-                        <button class="btn btn-primary w-100" type="submit">Sign in</button>
+                        <button class="btn btn-primary w-100" type="submit">Masuk</button>
 
                         <div class="col-12 text-center mt-3">
-                            <p class="mb-0 mt-3"><small class="text-dark me-2">Don't have an account ?</small> <a href="{{ route('register') }}" class="text-dark fw-bold">Sign Up</a></p>
+                            <p class="mb-0 mt-3"><small class="text-dark me-2">Belum punya akun?</small> <a href="{{ route('register') }}" class="text-dark fw-bold">Daftar</a></p>
                         </div><!--end col-->
 
                         <p class="mb-0 text-muted mt-3 text-center">© <script>document.write(new Date().getFullYear())</script> Mountaineer.</p>
