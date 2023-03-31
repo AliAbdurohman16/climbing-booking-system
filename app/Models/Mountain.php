0415ec390e0d7020052723b0e6ab485e7170d6d5
AliAbdurohman16 <alyabdurohman16@gmail.com>
2023-04-01 01:00:38 +0700

    update models mountain :rocket:

--- app/Models/Mountain.php
+++ app/Models/Mountain.php
@@ -10,10 +10,6 @@ class Mountain extends Model
 {
     use HasFactory;
 
-    protected $table = 'mountain';
-    protected $guarded = 'id';
-    protected $primaryKey = 'id';
-    public $timestamps = true;
     protected $fillable = [ 'user_id', 'name', 'thumbnail', 'description', 'address', 'price'];
 
     public function user()
